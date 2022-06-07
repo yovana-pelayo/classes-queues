6d45a3e9ec5cef19f5649afa21817151e1bc6c04
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-07 15:34:09 -0700

    created the 3 functions

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ class Stack {
       this.#Stack = animal;
     }
   }
+  //methods
+
+  push(item) {
+    this.#Stack.push(item);
+  }
+
+  pop() {
+    this.#Stack.shift();
+  }
+
+  peek() {
+    this.#Stack[0];
+  }
+  // properties
 }
 
 class Queue {}
